85e7318a563e840ee301b1b5ae8db5caedb5ac2d
Mark Manipula <markmikomanipula@gmail.com>
2024-12-24 01:51:15 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import com.interview.prep.mediums.grind75.week1.MaximumSubarray;
 public class Main {
 
     public static void main(String[] args) {
-        MaximumSubarray maximumSubarray = new MaximumSubarray();
-        System.out.println(maximumSubarray.maxSubArray(new int[] {-2,1,-3,4,-1,2,1,-5,4}));
+
     }
 }
